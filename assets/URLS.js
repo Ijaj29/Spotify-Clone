808fbab186de81ced288debda4fef194d5fe2cd4
Ijaj97 <124039753+Ijaj97@users.noreply.github.com>
2024-07-06 22:10:38 +0530

    Updates

---
+++ assets/URLS.js
@@ -0,0 +1,54 @@
+const RowData = [
+  {
+    title: 'Popular This Week',
+    movie_data: [
+      {
+        img: 'javan.jpg',
+        video: '../../assets/dunki.mp4',
+      },
+      {
+        img: 'url',
+        video: 'dunki.mp4',
+      },
+      {
+        img: 'url',
+        video: 'dunki.mp4',
+      },
+    ]
+  },
+  {
+    title: 'Trending',
+    url: '/discover/movie?sort_by=popularity.desc',
+  },
+  {
+    title: 'Action',
+    url: '/movie/top_rated',
+  },
+  {
+    title: 'Drama',
+    url: '/discover/movie?with_genres=28',
+  },
+  {
+    title: 'Horror',
+    url: '/discover/movie?with_genres=35',
+  },
+  {
+    title: 'Romantic',
+    url: '/discover/movie?with_genres=27',
+  },
+  {
+    title: 'Romanic',
+    url: '/discover/movie?with_genres=10749',
+  },
+  {
+    title: 'Romantic',
+    url: '/discover/movie?with_genres=99',
+  },
+  {
+    title: 'Romantic',
+    url: '/discover/movie?with_genres=878',
+  }
+];
+
+
+export default RowData;
